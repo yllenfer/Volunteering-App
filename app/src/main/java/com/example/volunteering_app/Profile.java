5aa4bc56bf1c45c4c0f719e5c55fe34abd6105f2
Yllen Fernandez <fer19020@byui.edu>
2023-04-18 23:56:09 +0200

    Progress on Profile activity layout

--- app/src/main/java/com/example/volunteering_app/Profile.java
+++ app/src/main/java/com/example/volunteering_app/Profile.java
@@ -1,14 +1,122 @@
 package com.example.volunteering_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class Profile extends AppCompatActivity {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private TextView textViewCity; // Variable to reference the city TextView
+    private TextView textViewCountry; // Variable to reference the country TextView
+
+    private void requestLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            getLastKnownLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastKnownLocation();
+            } else {
+                // Permission denied
+            }
+        }
+    }
+
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private void getLastKnownLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, location -> {
+                        if (location != null) {
+                            double latitude = location.getLatitude();
+                            double longitude = location.getLongitude();
+                            getAddressFromLocation(latitude, longitude);
+                        }
+                    });
+        }
+    }
+
+    private void getAddressFromLocation(double latitude, double longitude) {
+        // Check if the Geocoder is available
+        if (!Geocoder.isPresent()) {
+            // Handle the case where the Geocoder is not available
+            System.out.println("Here  I am");
+            return;
+        }
+
+
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                Address address = addresses.get(0);
+                String city = address.getLocality();
+                String country = address.getCountryName();
+//                System.out.println(city);
+
+                // Check if the city and country are not null
+                if (city != null && country != null) {
+                    // Display the city and country in the TextViews
+                    textViewCity.setText(city);
+                    textViewCountry.setText(country);
+                } else {
+                    // Handle the case where the city or country is null
+                }
+            } else {
+                // Handle the case where the Geocoder returns no results
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception
+        }
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        requestLocationPermissions();
+
+        textViewCity = findViewById(R.id.cityLocation);
+        textViewCountry = findViewById(R.id.countryLocation);
+
+
+
     }
 }
