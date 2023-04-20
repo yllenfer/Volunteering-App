5d8ba7a4b4158cb8dbc33052264d6c5d12c1f190
Yllen Fernandez <fer19020@byui.edu>
2023-04-20 17:17:43 +0200

    Fully connected to Firebase and users are created successfully.

--- app/src/main/java/com/example/volunteering_app/Register.java
+++ app/src/main/java/com/example/volunteering_app/Register.java
@@ -1,14 +1,81 @@
 package com.example.volunteering_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+
+    TextInputEditText textRegisterEmail, textRegisterPassword;
+    Button regButton;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        textRegisterEmail = findViewById(R.id.editTextRegisterEmail);
+        textRegisterPassword = findViewById(R.id.editTextRegisterPassword);
+        regButton = findViewById(R.id.registerButton);
+        mAuth = FirebaseAuth.getInstance();
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = String.valueOf(textRegisterEmail.getText());
+                password = String.valueOf(textRegisterPassword.getText());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Account created", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                                    startActivity(intent);
+                                    finish();
+
+
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Nothing Happened!.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+
+
+
+            }
+        });
     }
 }
